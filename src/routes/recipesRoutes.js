726a406ee84ab40412f04d2a8d72ea52ea8adf09
Eugene Mukhin <eug.muhin@gmail.com>
2026-06-12 18:36:13 +0300

    Add authenticate middleware and clean up recipesRoutes.js imports

--- src/routes/recipesRoutes.js
+++ src/routes/recipesRoutes.js
@@ -1,26 +1,18 @@
-import { Router } from 'express';
-import { celebrate } from 'celebrate';
+import { Router } from 'express'
+import { celebrate } from 'celebrate'
 
 import {
   getAllRecipes,
   getFavoriteRecipes,
-} from '../controllers/recipesController.js';
+} from '../controllers/recipesController.js'
 
-import { getAllRecipesSchema } from '../validations/recipesValidation.js';
-import { authenticate } from '../middlewares/authenticate.js';
+import { getAllRecipesSchema } from '../validations/recipesValidation.js'
+import { authenticate } from '../middleware/authenticate.js'
 
-const router = Router();
+const router = Router()
 
-router.get(
-  '/recipes',
-  celebrate(getAllRecipesSchema),
-  getAllRecipes,
-);
+router.get('/recipes', celebrate(getAllRecipesSchema), getAllRecipes)
 
-router.get(
-  '/recipes/favorites',
-  authenticate,
-  getFavoriteRecipes,
-);
+router.get('/recipes/favorites', authenticate, getFavoriteRecipes)
 
-export default router;
+export default router
